b3ebb7a9fd079e160e7d6848f7cf63f975aa51a6
Chip Weinberger <weinberger.c@gmail.com>
2024-03-29 12:06:15 -0700

    [Improve] only stopScan if currently scanning. fixes 'could not find callback wrapper' log

--- lib/src/flutter_blue_plus.dart
+++ lib/src/flutter_blue_plus.dart
@@ -355,7 +355,11 @@ class FlutterBluePlus {
   static Future<void> stopScan() async {
     _Mutex mtx = _MutexFactory.getMutexForKey("scan");
     await mtx.take();
-    await _stopScan();
+    if (isScanningNow) {
+      await _stopScan();
+    } else if (_logLevel.index >= LogLevel.info.index) {
+      print("[FBP] stopScan: already stopped");
+    }
     mtx.give();
   }
 
@@ -442,7 +446,7 @@ class FlutterBluePlus {
       }
       if (r.adapterState == BmAdapterStateEnum.on) {
         for (DeviceIdentifier d in _autoConnect) {
-          BluetoothDevice(remoteId: d).connect(autoConnect: true, mtu: null).onError((e,s) {
+          BluetoothDevice(remoteId: d).connect(autoConnect: true, mtu: null).onError((e, s) {
             if (logLevel != LogLevel.none) {
               print("[FBP] [AutoConnect] connection failed: $e");
             }
@@ -484,7 +488,7 @@ class FlutterBluePlus {
           if (_autoConnect.contains(r.remoteId)) {
             if (_adapterStateNow == BmAdapterStateEnum.on) {
               var d = BluetoothDevice(remoteId: r.remoteId);
-              d.connect(autoConnect: true, mtu: null).onError((e,s) {
+              d.connect(autoConnect: true, mtu: null).onError((e, s) {
                 if (logLevel != LogLevel.none) {
                   print("[FBP] [AutoConnect] connection failed: $e");
                 }
